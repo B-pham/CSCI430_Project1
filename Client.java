8012859e3e1e869305151666c1fb92e1b0de2577
B-pham <59837130+B-pham@users.noreply.github.com>
2021-02-11 18:53:26 -0600

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Client implements Serializable{
+
+
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String address;
+    private String phone;
+    private String id;
+    
+    private List shopCart = new LinkedList();    
+    private List transactions = new LinkedList();
+    
+    public Client(String name, String address, String Phone, String id){
+        this.name = name;
+        this.address = address;
+        this.phone = Phone;
+        this.id = id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    } 
+
+    @Override
+    public String toString() {
+        return "Client{" + "name=" + name + ", address=" + address + ", phone=" + phone + ", id=" + id + ", shopCart=" + shopCart + ", transactions=" + transactions + '}';
+    }    
+}
